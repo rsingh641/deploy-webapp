6784116fef9b10c1fc213bbf782cde2751a85596
rsingh641 <rsingh641@gmail.com>
2024-08-26 20:42:57 +0530

    fix for setting oracle creds in spring app

--- src/deploy/deploy_app.sh
+++ src/deploy/deploy_app.sh
@@ -20,6 +20,12 @@ replace_properties
 logger "INFO" "Deploying Webservice artifacts to Spring app service instance"
 deploy_webservice_artifacts
 
+logger "INFO" "Fetching DB2 certificates from Keyvault and providing to Spring app service instance"
+fetch_and_provide_db2_certs
+
+logger "INFO" "Fetching Oracle credentials from Keyvault and providing to Spring app service instance"
+fetch_and_provide_oracle_creds
+
 logger "INFO" "Deploying UI artifacts to Webapp service instance"
 deploy_ui_artifact
 
